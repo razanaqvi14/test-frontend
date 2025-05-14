9f56ab4f7408c4f2498a7c29b85bfab8562f91f9
razanaqvi <razanaqvi2004@gmail.com>
2025-05-15 02:05:56 +0500

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [message, setMessage] = useState("Loading...");
+
+  useEffect(() => {
+    fetch("https://test-backend-ukft.onrender.com")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch((err) => setMessage("Error contacting backend"));
+  }, []);
+
+  return (
+    <main>
+      <h1>Next.js Frontend</h1>
+      <p>{message}</p>
+    </main>
+  );
+}
